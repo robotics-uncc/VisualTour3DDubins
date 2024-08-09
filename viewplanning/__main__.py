462cd7ac1c5e29afc7a37f442da64750ef4eee1c
chague-uncc <chague@uncc.edu>
2024-08-09 16:18:07 -0400

    config and main changes

--- viewplanning/__main__.py
+++ viewplanning/__main__.py
@@ -1,36 +1,38 @@
 import argparse
 from viewplanning.configuration import ConfigurationFactory
-from viewplanning.cli import RunExperiments, Create, TestExperiments, View, Subapplication
+from viewplanning.cli import RunExperiments, Create, TestExperiments, View
 import logging
+from logging.handlers import RotatingFileHandler
 import sys
+import os
 
 
 def main():
-    subCommands: 'list[Subapplication]' = [
+    subCommands = [
         RunExperiments(),
         Create(),
         TestExperiments(),
         View()
     ]
-    parser = argparse.ArgumentParser(prog='viewplanning', description='Solves the view planning tour problem for the 3D Dubins airplane.')
-    parser.add_argument('-c', '--config', type=str, default='./viewplanning/config.yaml', help='yaml config file')
+    parser = argparse.ArgumentParser(prog='viewplanning', help='solve the view planning problem for the 3D Dubins airplane')
+    parser.add_argument('-c', '--config', type=str, default='./viewplanning/config.yaml', help='yaml configuration file for the solver')
     subparsers = parser.add_subparsers()
     for subCommand in subCommands:
-        subparser = subparsers.add_parser(subCommand.key, description=subCommand.description)
+        subparser = subparsers.add_parser(subCommand.key)
         subCommand.modifyParser(subparser)
-
     args = parser.parse_args()
     config = ConfigurationFactory.getInstance(args.config)['logging']
     logger = logging.getLogger()
     for log in config['handlers']:
         if log['out'] == 'stdout':
             stream = sys.stdout
+            handler = logging.StreamHandler(stream)
         elif log['out'] == 'stderr':
             stream = sys.stderr
+            handler = logging.StreamHandler(stream)
         else:
-            stream = open(log['out'], log['mode'])
+            handler = RotatingFileHandler(log['out'], log['mode'], maxBytes=100 * 2 ** 20, backupCount=3)
         form = logging.Formatter(log['format'])
-        handler = logging.StreamHandler(stream)
         handler.setLevel(log['level'])
         handler.setFormatter(form)
         logger.addHandler(handler)
@@ -38,7 +40,10 @@ def main():
     if 'application' not in args._get_args():
         parser.print_help()
         return
-    args.application(args)
+    try:
+        args.application(args)
+    except:
+        logger.critical('Critial Error')
 
 
 if __name__ == '__main__':
